528da55a762f03c89355af652e66e4a86ca8f8ce
Denis <161995304+DenisPronin125@users.noreply.github.com>
2024-03-08 19:58:50 +1100

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-var name = 'Денис';
-console.log(name);
+import java.util.Scanner;
+
+public class GuessNumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int secretNumber = (int) (Math.random() * 100); // Генерируем случайное число от 0 до 99
+        int guess = -1;
+
+        System.out.println("Добро пожаловать в игру 'Угадай число'!");
+
+        while (guess != secretNumber) {
+            System.out.print("Угадайте число от 0 до 99: ");
+            guess = scanner.nextInt();
+
+            if (guess < secretNumber) {
+                System.out.println("Загаданное число больше!");
+            } else if (guess > secretNumber) {
+                System.out.println("Загаданное число меньше!");
+            }
+        }
+
+        System.out.println("Поздравляем, вы угадали число! Загаданное число: " + secretNumber);
+
+        scanner.close();
+    }
+}
+
